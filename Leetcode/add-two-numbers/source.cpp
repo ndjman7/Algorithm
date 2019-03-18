76cc2a2bd693c0ad8437bf4d42291a605c08a417
ndjman7 <ndjman7@gmail.com>
2019-03-18 10:28:31 +0900

    Letcode: Add add-two-numbers problem solution
    link: https://leetcode.com/problems/add-two-numbers/

--- Leetcode/add-two-numbers/source.cpp
+++ Leetcode/add-two-numbers/source.cpp
@@ -9,32 +9,46 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // 결과로 반환할 ListNode.
         ListNode* root;
-        ListNode node(l1->val + l2->val);
+        // 첫번째 Node를 만듬.
+        ListNode* node = new ListNode(l1->val + l2->val);
+        if(node->val / 10) {
+            ListNode* new_node = new ListNode(node->val / 10);
+            node->val %= 10;
+            node->next = new_node;
+        }
         l1 = l1->next;
         l2 = l2->next;
-        root = &node;
+        root = node;
+        
+        // 둘 중 하나라도 NULL이 아닐때 까지 계속 반복.
         while(l1 != NULL || l2 != NULL) {
-            if(node.next) {
-                node = *(node.next);
-                node.val = node.val + l1->val + l2->val;
+            // node의 next가 NULL이 아니라면, 그 전에 carry된 값이 있다는 것.
+            int l1_val = l1 ? l1->val : 0;
+            int l2_val = l2 ? l2->val : 0;
+            if(node->next) {
+                node = node->next;
+                node->val = node->val + l1_val + l2_val;
             }
             else {
-                ListNode new_node(l1->val + l2->val);
-                node.next = &new_node;
-                node = *(node.next);
+                ListNode* new_node = new ListNode(l1_val + l2_val);
+                node->next = new_node;
+                node = node->next;
             }
-            
-            if(node.val / 10) {
-                node.val %= 10;
-                ListNode new_node(node.val / 10);
-                node.next = &new_node;
+
+            if(node->val / 10) {
+                ListNode* new_node = new ListNode(node->val / 10);
+                node->val %= 10;
+                node->next = new_node;
             }
-            
-            l1 = l1->next;
-            l2 = l2->next;
+
+            if(l1 != NULL)
+                l1 = l1->next;
+            if(l2 != NULL)
+                l2 = l2->next;
 
         }
-        return root;  
+        return root;
     }
 };
