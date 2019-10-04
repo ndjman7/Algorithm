b1fa52bc94bd6aed09a6ee14004f937b075519f0
daejin <ndjman7@gmail.com>
2019-10-04 10:11:15 +0900

    Leetcode: Add di-string-match problem solution
    link: https://leetcode.com/problems/di-string-match/

---
+++ Leetcode/di-string-match/source.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diStringMatch(self, S: str) -> List[int]:
+        lo, hi = 0, len(S)
+        ans = []
+
+        for c in S:
+            if c == 'I':
+                ans.append(lo)
+                lo += 1
+
+            else:
+                ans.append(hi)
+                hi -= 1
+
+        return ans + [lo]
