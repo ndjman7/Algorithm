5a085ea060d8f96992b12c49276fffc492a36379
pando <ndjman7@gmail.com>
2019-12-24 08:33:25 +0900

    Leetcode: Add convert-binary-number-in-a-linked-list-to-integer problem solution
    link: https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/

---
+++ Leetcode/convert-binary-number-in-a-linked-list-to-integer/source.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+
+        answer = 0
+        while head is not None:
+            answer *= 2
+            answer += head.val
+            head = head.next
+
+        return answer
