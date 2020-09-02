b7d9833c65ec34200f4c0e0d3af4e3ca0ba24a75
pando <ndjman7@gmail.com>
2020-09-02 19:10:21 +0900

    Leetcode: Add three-consecutive-odds problem solution
    link: https://leetcode.com/problems/three-consecutive-odds/

---
+++ three-consecutive-odds/source.py
@@ -0,0 +1,11 @@
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        odd_count = 0
+        for el in arr:
+            if el % 2:
+                odd_count += 1
+                if odd_count == 3:
+                    return True
+            else:
+                odd_count = 0
+        return False
